b07ab809bb7d6e5700011de66d24e12b2ba8a17f
PurPul <axbergsamuel@gmail.com>
2024-10-07 19:39:15 +0300

    Exercise 4.7

--- blog_list/utils/list_helper.js
+++ blog_list/utils/list_helper.js
@@ -25,21 +25,40 @@ const mostBlogs = (blogs) => {
     const authors = lodash.countBy(blogs, (blog)=>{
         return blog.author
     })
-    console.log(authors);
     
     if (Object.keys(authors).length == 0) {
         return null
     }
-    const most_author = Object.keys(authors).reduce((a, b) => authors[a] < authors[b] ? b : a);
+    const most_author = Object.keys(authors).reduce((a, b) => authors[a] < authors[b] ? b : a)
     return {
         author : most_author,
         blogs : authors[most_author]
     }
 }
 
+const mostLikes = (blogs) => {
+    if (blogs.length == 0) {
+        return null
+    }
+
+    let authors = blogs.map(({author}) => (author))
+    authors = lodash.uniq(authors)
+
+    let author_likes = []
+    authors.forEach((author) => {
+        const likes = lodash.sumBy(lodash.filter(blogs, {author : author}), 'likes')
+        author_likes.push({author: author, likes: likes})
+    })
+    
+    const most_liked_author = author_likes.reduce((a, b) => a.likes < b.likes ? b : a)
+
+    return most_liked_author
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
