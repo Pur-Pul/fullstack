9a531fff0601956f9c27502de212b9721440991f
PurPul <axbergsamuel@gmail.com>
2024-11-22 13:30:34 +0200

    Exercise 7.7

---
+++ country-hook/src/hooks/index.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useCountry = (name) => {
+    const [country, setCountry] = useState(null)
+
+    useEffect(() => {
+        if (name !== '') {
+            axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+                .then((response) => {
+                    setCountry({ found:true, data:
+                        { 
+                            name: response.data.name.common,
+                            capital: response.data.capital,
+                            population: response.data.population,
+                            flag: response.data.flags.png
+                        } 
+                    })
+                })
+                .catch((error) => {
+                    setCountry(error)
+                    
+                })
+                
+        }
+    }, [name])
+    
+    return country
+}
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+  
+    const onChange = (event) => {
+      setValue(event.target.value)
+    }
+  
+    return {
+      type,
+      value,
+      onChange
+    }
+  }
