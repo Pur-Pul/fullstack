a7830a66c0f379ec1fcc53a53b476d524747654b
PurPul <axbergsamuel@gmail.com>
2024-12-17 16:06:25 +0200

    Exercise 8.3

--- library-backend/library-backend.js
+++ library-backend/library-backend.js
@@ -105,11 +105,19 @@ const typeDefs = `
     id: ID
     genres: [String]
   }
-
+    
+  type Author {
+    name: String
+    id: ID
+    born: Int
+    bookCount: Int
+  }
+  
   type Query {
     bookCount: Int
     authorCount: Int
     allBooks: [Book]
+    allAuthors: [Author]
   }
 `
 
@@ -117,7 +125,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter((book) => book.author === root.name).length
   }
 }
 
