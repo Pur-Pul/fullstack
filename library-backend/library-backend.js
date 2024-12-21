701df578ce7c290ce77bd1cf199e872025bfc3f0
PurPul <axbergsamuel@gmail.com>
2024-12-21 12:11:45 +0200

    Exercise 8.6

--- library-backend/library-backend.js
+++ library-backend/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -119,6 +120,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book]
     allAuthors: [Author]
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String
+      published: Int
+      genres: [String]
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -139,6 +149,16 @@ const resolvers = {
   },
   Author: {
     bookCount: (root) => books.filter((book) => book.author === root.name).length
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      if (book.author && !authors.some(author => author.name === book.author)) {
+        authors = authors.concat({ name:book.author, id: uuid() })
+      }
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
