f0cb0e659e5ca1629095f752572eff4e107fa91d
PurPul <axbergsamuel@gmail.com>
2025-01-21 12:08:17 +0200

    Exercise 9.9

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -3,11 +3,11 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173"}))
+app.use(cors({origin: "http://localhost:5173"}));
 const PORT = 3000;
 
 const api = express.Router();
-app.use('/api', api)
+app.use('/api', api);
 
 api.get('/ping', (_req, res) => {
   console.log('someone pinged here');
