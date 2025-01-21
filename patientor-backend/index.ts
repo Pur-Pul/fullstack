0a3e44da10580f6c1109dce09607d5652988c6a1
PurPul <axbergsamuel@gmail.com>
2025-01-21 12:04:14 +0200

    Exercise 9.8

---
+++ patientor-backend/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors({origin: "http://localhost:5173"}))
+const PORT = 3000;
+
+const api = express.Router();
+app.use('/api', api)
+
+api.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
