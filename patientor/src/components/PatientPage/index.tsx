1c1f80c0f670197d1c3c5c1cd48bf1d078c9fb44
PurPul <axbergsamuel@gmail.com>
2025-01-29 14:06:20 +0200

    Exercise 9.24

--- patientor/src/components/PatientPage/index.tsx
+++ patientor/src/components/PatientPage/index.tsx
@@ -5,8 +5,20 @@ import MaleIcon from '@mui/icons-material/Male';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import patientService from '../../services/patients';
 import { useEffect, useState } from "react";
+import { Entry } from "../../types";
 
+interface EntryProps {
+    entries: Entry[]
+};
 
+const Entries = ({ entries }: EntryProps) => {
+    return entries.map(entry => 
+        <div key={ entry.id }>
+            <p>{ entry.date } <i>{ entry.description }</i></p>
+            { entry.diagnosisCodes ? <ul>{ entry.diagnosisCodes.map(code => <li key={ code }>{ code }</li>) }</ul> : null }
+        </div>
+    );
+};
 
 const PatientPage = () => {
     const { id } = useParams()
@@ -15,16 +27,16 @@ const PatientPage = () => {
     useEffect(() => {
         if (id) {
             const getPatient = async () => {
-                const patient = await patientService.get(id)
-                setPatient(patient)
+                const patient = await patientService.get(id);
+                setPatient(patient);
             }
-            getPatient()
+            getPatient();
         }
 
     }, [id]);
     
     if (!patient) { return null }
-    let GenderIcon = undefined
+    let GenderIcon = undefined;
     switch(patient.gender) {
         case "male": {
             GenderIcon = MaleIcon
@@ -45,8 +57,9 @@ const PatientPage = () => {
             <span>ssn: { patient.ssn ? patient.ssn : '-' }</span><br/>
             <span>date of birth: { patient.dateOfBirth ? patient.dateOfBirth : '-' }</span><br/>
             <span>occupation: { patient.occupation }</span>
+            <Entries entries = { patient.entries }/>
         </div>
-    )
+    );
 };
 
 export default PatientPage;
