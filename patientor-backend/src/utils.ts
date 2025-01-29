00982a928751b3dfe23123033f1a7471e7f56e01
PurPul <axbergsamuel@gmail.com>
2025-01-29 13:20:44 +0200

    Exercise 9.23

--- patientor-backend/src/utils.ts
+++ patientor-backend/src/utils.ts
@@ -1,7 +1,35 @@
 import { Gender } from "./types";
 import z from "zod";
 
-export const entrySchema = z.object({})
+export const baseEntrySchema = z.object({
+    id: z.string(),
+    date: z.string().date(),
+    description: z.string(),
+    specialist: z.string(),
+    diagnosisCodes: z.array(z.string()).optional()
+});
+
+export const healthCheckEntrySchema = baseEntrySchema.extend({
+    type: z.literal("HealthCheck"),
+    healthCheckRating: z.number()
+});
+
+export const occupationalHealthcareEntrySchema = baseEntrySchema.extend({
+    type: z.literal("OccupationalHealthcare"),
+    employerName: z.string(),
+    sickLeave: z.optional(z.object({
+        startDate: z.string().date(),
+        endDate: z.string().date()
+    }))
+});
+
+export const hospitalEntrySchema = baseEntrySchema.extend({
+    type: z.literal("Hospital"),
+    discharge: z.object({
+        date: z.string().date(),
+        criteria: z.string(),
+    })
+})
 
 export const newPatientSchema = z.object({
     name: z.string(),
@@ -9,5 +37,5 @@ export const newPatientSchema = z.object({
     ssn: z.string(),
     gender: z.nativeEnum(Gender),
     occupation: z.string(),
-    entries: z.array(entrySchema)
+    entries: z.array(z.union([hospitalEntrySchema, occupationalHealthcareEntrySchema, healthCheckEntrySchema]))
 });
